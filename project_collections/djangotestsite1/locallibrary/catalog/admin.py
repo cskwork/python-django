e82a3ab6a9cc1542937018f0267b859e94205f39
Gitter <csk917@hotmail.com>
2021-02-12 14:38:51 +0900

    change project

--- project_collections/djangotestsite1/locallibrary/catalog/admin.py
+++ project_collections/djangotestsite1/locallibrary/catalog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance,Language
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+#admin.site.register(BookInstance)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+   list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+  # inlines = [BooksInstanceInline]
+   #pass
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    #pass
+    list_display = ('title', 'author', 'display_genre')
+    list_filter = ('genre','author')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    #pass
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        ('기본', {
+            'fields': ('book', 'status','due_back')
+        }),
+       
+    )
+
--- project_collections/djangotestsite1/locallibrary/catalog/models.py
+++ project_collections/djangotestsite1/locallibrary/catalog/models.py
@@ -11,13 +11,20 @@ class Genre(models.Model):
         """String for representing the Model object."""
         return self.name
 
+class Language(models.Model):
+
+    language = models.CharField(max_length=200, help_text='언어를 추가하세요')
+
+    def __str__(self):
+        return self.language
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='제목')
 
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name='저자')
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True,
@@ -27,6 +34,9 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -34,6 +44,40 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+#Not recommended due to DB cost
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+"""
+    def testFilter(self):
+        a = Book(author = 'tom', 
+            summary = 'example@email.com',
+            isbn ='test3', 
+            genre = 'test')
+        a.save()
+        #a.delete()
+        print(a.id, a.pk)
+        print(a.summary)
+"""
+
+#    print (reverse('book-detail', args=[str(1)]))
+
+
+# TEST ADD  
+"""
+    def getWildBooks(self):
+        wild_books = Book.objects.filter(title__contains='wild')
+        number_wild_books = wild_books.count()
+        return wild_books
+"""
+    
+
+
+  #  [Out] 2, 2
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -57,11 +101,20 @@ class BookInstance(models.Model):
         help_text='Book availability',
     )
 
+    #record = BookInstance(my_field_name="Instance #1")
+#
+   # print(record)
+   # print(record.id)
+
+
     class Meta:
         ordering = ['due_back']
 
     def __str__(self):
-        """String for representing the Model object."""
+        """String for representing the Model object."""   
+
+        #Return double pk 
+        #print(f'{self.id} ({self.book.title})')
         return f'{self.id} ({self.book.title})'
 
 class Author(models.Model):
